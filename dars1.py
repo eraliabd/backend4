565c1ae4aebf0158c200d634fc3ad49ab45de2db
eraliabd <eraliabdinazarov@gmail.com>
2024-11-30 20:33:38 +0500

    dars1 faylimni githubga yukladim

---
+++ dars1.py
@@ -0,0 +1,51 @@
+# print("salom")
+# salom = "salom"
+# print(type(salom))
+son = 5
+print(type(son))
+
+son1 = 4.6
+# print(type(son1))
+# pydroid
+
+# int - butun sonlar -...-3, -2, -1, 0, 1, 2, 3, ...1000000, ...
+# float - kasr sonlar 2.3, 1.5, -7.4
+
+# print(son + son1)  # 9.6
+
+
+# bool - True va False ()
+
+# l = [[1,2,3, [56]], [4,5]]
+
+# print(l[0][0])
+
+
+# print("5*5:", 5*5)
+#
+# print("5/2:", 5/2) # 2.5
+# print("5//2:", 5//2) # 2 --> butun bo'lish
+#
+# print("5-2:", 5-2) print()
+# print("5+2:", 5+2)
+
+# pydroid
+# 40 minut, hamma chiqib 5+5 10 minut
+
+# print3.12 --> python3
+# python2 print "salom"
+#
+# salom = "mening ismim Shox"
+# print(salom)
+# Ctrl+/
+
+# 1
+# 1-savol
+# 2-savol
+# 2
+# 1-savol
+# 2-savol
+# 3
+# 1-savol
+# 2-savol
+
